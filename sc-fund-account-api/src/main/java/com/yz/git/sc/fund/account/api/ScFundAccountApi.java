fbd4d62e73050e1097a8635f89cd612bbc4c3856
xuyang <xuyang7@le.com>
2019-08-19 18:08:23 +0800

    添加查询产品列表接口 未实现 编译通过

--- sc-fund-account-api/src/main/java/com/yz/git/sc/fund/account/api/ScFundAccountApi.java
+++ sc-fund-account-api/src/main/java/com/yz/git/sc/fund/account/api/ScFundAccountApi.java
@@ -1,8 +1,19 @@
 package com.yz.git.sc.fund.account.api;
 
+import com.yz.git.sc.fund.account.common.message.Message;
+import com.yz.git.sc.fund.account.param.ProductInfo;
+
+import java.util.List;
+
 /**
  * @author xuyang
  * @date 2019/08/19
  */
 public interface ScFundAccountApi {
+    /**
+     * 查询基金列表
+     * @return
+     */
+    Message<List<ProductInfo>> queyProductList();
+
 }
---
+++ sc-fund-account-service/src/main/java/com/yz/git/sc/fund/account/api/impl/ScFundAccountApiImpl.java
@@ -0,0 +1,18 @@
+package com.yz.git.sc.fund.account.api.impl;
+
+import com.yz.git.sc.fund.account.api.ScFundAccountApi;
+import com.yz.git.sc.fund.account.common.message.Message;
+import com.yz.git.sc.fund.account.param.ProductInfo;
+
+import java.util.List;
+
+/**
+ * @author hexinlong
+ * @date 2019/08/19
+ */
+public class ScFundAccountApiImpl implements ScFundAccountApi{
+    @Override
+    public Message<List<ProductInfo>> queyProductList() {
+        return null;
+    }
+}
